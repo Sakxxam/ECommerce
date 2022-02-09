bf1b54e192fb8705b0756c25b66f68de0a4f9dc4
Saksham Sharma <98305931+Sakxxam@users.noreply.github.com>
2022-02-09 20:28:40 +0530

    final

---
+++ Book Shopping Project/wwwroot/js/product.js
@@ -0,0 +1,61 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+})
+function loadDataTable() {
+    dataTable = $('#tblData').dataTable({
+        "ajax": {
+            "url":"/Admin/Product/GetAll"
+        },
+        "columns": [
+            { "data": "title", "width": "15%" },
+            { "data": "description", "width": "20%" },
+            { "data": "author", "width": "15%" },
+            { "data": "isbn", "width": "15%" },
+            { "data": "price", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+
+                       <div class="text-center">
+                       <a class="btn btn-primary" href="/Admin/Product/Upsert/${data}">
+                       <i class="fas fa-edit"></i>
+                       </a>
+                       <a class="btn btn-danger" onclick=Delete("/Admin/Product/Delete/${data}")>
+                       <i class="fas fa-trash-alt"></i>
+                       </a>
+                       </div>
+
+                    `;
+                }
+            }
+        ]
+    })
+}
+
+function Delete(url) {
+    swal({
+        title: "Want to delete data ?",
+        icon: "warning",
+        text: "Delete Information !",
+        buttons: true,
+        dangerModel: true
+    }).then((willdelete) => {
+        if (willdelete) {
+            $.ajax({
+                url: url,
+                type: "DELETE",
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                         dataTable.api().ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            })
+        }
+    })
+}
---
+++ Book Shopping Project/wwwroot/js/user.js
@@ -0,0 +1,76 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+})
+function loadDataTable() {
+    dataTable = $('#tblData').dataTable({
+        "ajax": {
+            "url": "/Admin/User/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width":"15%" },
+            { "data": "email", "width": "20%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "company.name", "width": "15%" },
+            { "data": "role", "width": "15%" },
+            {
+                "data": {
+                    id:"id",lockoutEnd:"lockoutEnd"
+                },
+                "render":function(data) {
+                    var today = new Date().getTime();
+
+                    var lockOut = new Date(data.lockoutEnd).getTime();
+
+                    if (lockOut > today) {
+                        return `
+
+                       <div class="text-center">
+                      <a onclick=LockUnLock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer">
+                        UnLock
+                        </a>
+                       </div>
+
+                    `;
+                    }
+                    else
+                    {
+                        //User UnLock
+
+                        return `
+
+                       <div class="text-center">
+                      <a onclick=LockUnLock('${data.id}') class="btn btn-success text-white" style="cursor:pointer">
+                        Lock
+                        </a>
+                       </div>
+
+                    `;
+
+                    }
+
+                }
+            }
+        ]
+    })
+}
+
+function LockUnLock(id) {
+   // alert('call');
+
+    $.ajax({
+        type: "POST",
+        url: "/Admin/User/LockUnlock",
+        data: JSON.stringify(id),
+        contentType: "application/json",
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                dataTable.api().ajax.reload();
+            }
+            else {
+                toastr.error(data.message);
+            }
+        }
+    })
+}
