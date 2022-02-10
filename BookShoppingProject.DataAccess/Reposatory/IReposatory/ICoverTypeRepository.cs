ee3a0b4510162c6c131cf571521a80973041a28b
Saksham Sharma <98305931+Sakxxam@users.noreply.github.com>
2022-02-10 11:19:36 +0530

    final

---
+++ BookShoppingProject.DataAccess/Reposatory/IReposatory/ICoverTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookShoppingProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShoppingProject.DataAccess.Repository.IRepository
+{
+    public interface ICoverTypeRepository:IRepository<CoverType>
+    {
+        void Update(CoverType coverType);
+    }
+}
---
+++ BookShoppingProject.DataAccess/Reposatory/IReposatory/IUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShoppingProject.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        ICompanyRepository Company { get; }
+        IApplicationUserRepository ApplicationUser { get; }
+        IProductRepository Product { get; }
+        ICategoryRepository Category { get;}
+
+        ISP_CALLS SP_CALLS { get; }
+        ICoverTypeRepository CoverType { get; }
+
+        IShoppingCartRepository ShoppingCart { get; }
+
+        IOrderHeaderRepository OrderHeader { get; }
+
+        IOrderDetailRepository OrderDetail { get; }
+
+        void Save();
+    }
+}
